0944bd4c649d219fcd54ab80209f4f64ffe10c6e
Sunil Shelke <shelkesunil072@gmail.com>
2025-12-20 18:14:55 +0530

    pp
    ne

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Merge_pdf from './pages/Merge_PDF/Merge_pdf'
+import Header from './componant/Header/Header'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home /> } />
       <Route path="/merge-pdf" element={<Merge_pdf />} />
--- src/componant/Header/Header.jsx
+++ src/componant/Header/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import './header.scss'
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="header-parent parent">
+        <div className="header-cont cont">
+          <div className="logo">
+            <h3>LOGO</h3>
+          </div>
+          <div className="menu">
+            <p>Home</p>
+            <p>About</p>
+            <p> Services</p>
+            <p>Portfolio</p>
+            <p>Blog</p>
+            <p>Contact</p>
+
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
